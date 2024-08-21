6cba3cb1d8965e73d9ed669855ef1572b055b59a
a23emijo <a23emijo@student.his.se>
2024-08-21 15:17:24 +0200

    Added buttons inside the teampanels and commented almost all code

--- src/ChoseTeamPanel.java
+++ src/ChoseTeamPanel.java
@@ -1,18 +1,40 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ChoseTeamPanel extends JPanel {
-
+public class ChoseTeamPanel extends JPanel implements ActionListener {
     private Dimension panelSize = new Dimension(200, 300);
-    private ChoseTeam1 panel1 = new ChoseTeam1();
-    private ChoseTeam2 panel2 = new ChoseTeam2();
+    private ChoseTeam1 homeTeam = new ChoseTeam1();
+    private ChoseTeam2 awayTeam = new ChoseTeam2();
+    private JButton loadHomeTeam = new JButton("Välj hemmalag!"); // Creates home team button
+    private JButton loadAwayTeam = new JButton("Välj bortalag!"); // Creates away team button
 
     public ChoseTeamPanel(){
+        // Sets the dimensions on the panels
+        homeTeam.setPreferredSize(panelSize);
+        awayTeam.setPreferredSize(panelSize);
 
-        panel1.setPreferredSize(panelSize);
-        panel2.setPreferredSize(panelSize);
+        // Adds the panels
+        this.add(homeTeam, BorderLayout.WEST);
+        this.add(awayTeam, BorderLayout.EAST);
 
-        this.add(panel1, BorderLayout.WEST);
-        this.add(panel2, BorderLayout.EAST);
+        // Adds action listeners
+        loadHomeTeam.addActionListener(this);
+        loadAwayTeam.addActionListener(this);
+
+        // Adds the buttons
+        homeTeam.add(loadHomeTeam);
+        awayTeam.add(loadAwayTeam);
+    }
+
+    // Listens to when a button is pressed
+    @Override
+    public void actionPerformed(ActionEvent e){
+        if (e.getSource() == loadHomeTeam) { // Clicked on "Välj hemmalag!"
+            System.out.println("Hemmalag!");
+        } else if (e.getSource() == loadAwayTeam){ // Clicked on "Välj bortlag!"
+            System.out.println("Bortalag!");
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import javax.swing.*;
 import java.awt.*;
 
 public class Main {
-
-    private JFrame frame = new JFrame("Basket lag");
-    private JLabel name = new JLabel();
-
+    private JFrame frame = new JFrame("Basketball"); // Creates a frame and sets a title
+    private JLabel label = new JLabel(); // Creates a label
 
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-
-        Main application = new Main(); // instantiates Main
-
-        application.execute();
-
+        Main application = new Main(); // Instantiates Main
+        application.execute(); // Runs the constructor
     }
 
     public void execute(){
-
-        frame.add(name, BorderLayout.NORTH);
-        frame.add(new ChoseTeamPanel(), BorderLayout.CENTER);
-
-        name.setText("Basket");
-        name.setFont(new Font("Ink Free", Font.BOLD, 60));
-
+        frame.add(label, BorderLayout.NORTH); // Adds the label to the frame
+        frame.add(new ChoseTeamPanel(), BorderLayout.CENTER); // Adds the panel that holds the teams to the frame
         frame.setBounds(100, 100, 900, 700); // Sets the size of the frame
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack(); // Sets the window to match a certain size
+        frame.setVisible(true); // Sets the window to be visible and in focus
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Exits the program when closed
 
+        // Creates rubric and sets a font
+        label.setText("Basket");
+        label.setFont(new Font("Ink Free", Font.BOLD, 60));
     }
 }
