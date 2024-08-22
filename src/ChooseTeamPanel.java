b97430c3ce04af47194d1601e2d0d93b441cb5f0
a23emijo <a23emijo@student.his.se>
2024-08-22 12:12:28 +0200

    Changed so the FileDialog opens with ".txt" selected and commented a lot of code

--- src/ChooseTeamPanel.java
+++ src/ChooseTeamPanel.java
@@ -2,20 +2,19 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ChooseTeamPanel extends JPanel {
-    private Dimension panelSize = new Dimension(200, 300);
-    private DisplayTextFilePanel homeTeam = new DisplayTextFilePanel("Välj hemmalag!", Color.GREEN);
-    private DisplayTextFilePanel awayTeam = new DisplayTextFilePanel("Välj bortalag!", Color.RED);
+    private Dimension panelSize = new Dimension(200, 300); // Sets the dimensions on the panels
+
+    // Creates the team buttons
+    private DisplayTextFilePanel homeTeam = new DisplayTextFilePanel("Choose hometeam!", Color.GREEN);
+    private DisplayTextFilePanel awayTeam = new DisplayTextFilePanel("Choose awayteam!", Color.RED);
 
     public ChooseTeamPanel(){
-        // Sets the dimensions on the panels
+        // Adds the dimensions to the panels
         homeTeam.setPreferredSize(panelSize);
         awayTeam.setPreferredSize(panelSize);
 
         // Adds the panels
         this.add(homeTeam, BorderLayout.WEST);
         this.add(awayTeam, BorderLayout.EAST);
-
-
     }
-
 }
